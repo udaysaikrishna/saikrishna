9969988dd6455784c646ef1b89cf70c9b41f392c
Sai Krishna Koniki <udaysaikrishna@gmail.com>
2023-08-17 14:45:50 -0400

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -49,8 +49,8 @@ if($_POST) {
       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+		//if ($mail) { echo "OK"; }
+      //else { echo "Something went wrong. Please try again."; }
 		
 	} # end if - no validation error
 
